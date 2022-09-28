30118e634f52728b78eb18ceb29518685115d951
我亦无他 <guofengwu1994@163.com>
2022-09-28 12:19:51 +0800

    feat: add test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import yayJpg from '../assets/yay.jpg';
+import yayJpg from '../assets/yay.jpg'
 
 export default function HomePage() {
   return (
@@ -10,6 +10,9 @@ export default function HomePage() {
       <p>
         To get started, edit <code>pages/index.tsx</code> and save to reload.
       </p>
+      <p className="font-sans ...">The quick brown fox ...</p>
+      <p className="font-serif ...">The quick brown fox ...</p>
+      <p className="font-mono ...">The quick brown fox ...</p>
     </div>
-  );
+  )
 }
