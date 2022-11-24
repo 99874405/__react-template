16c6ab2d74188adcdfac1638555fa51269e1beb5
我亦无他 <guofengwu1994@163.com>
2022-11-24 21:59:50 +0800

    chore: update

--- .umirc.ts
+++ .umirc.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'umi'
 export default defineConfig({
+  /* enable plugin */
+  devtool: process.env.NODE_ENV === 'development' ? 'source-map' : false,
   tailwindcss: {},
-  clientLoader: {},
-  plugins: ['@umijs/plugins/dist/tailwindcss'],
-  extraPostCSSPlugins: [require('tailwindcss'), require('autoprefixer')],
-  hash: true,
+  plugins: [
+    '@umijs/plugins/dist/tailwindcss',
+  ],
+  extraPostCSSPlugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('postcss-preset-env')(),
+  ],
 })
